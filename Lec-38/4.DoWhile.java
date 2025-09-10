6e1f25e551e245356484031c7ec20399098316d0
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-09-10 19:07:13 +0530

    Create 4.DoWhile.java

---
+++ Lec-38/4.DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String args[]){
+        int num=5;
+        do{
+            System.out.println(num);
+            num--;
+        }
+        while(num>1);
+    }
+}
