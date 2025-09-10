358c67c5473eb247751edc2a0b22be7f1d1e082c
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-09-10 19:01:03 +0530

    Create 2.ForLoop.java

---
+++ Lec-38/2.ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop{
+    public static void main(String args[]){
+        for (int i = 0; i < 3; i++) {
+            if (i == 1)
+                continue;
+            System.out.println(i);
+        }
+    }
+}
