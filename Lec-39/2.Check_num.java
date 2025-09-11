9b16b0cab2bd17ebbb0f80b4205317d8205411e6
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-09-11 21:50:54 +0530

    check num and print High/ low

---
+++ Lec-39/2.Check_num.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Check_num{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+            if (number > 5) {
+                System.out.println("High");
+            } else {
+                System.out.println("Low");
+            }
+        scanner.close();
+    }
+}
