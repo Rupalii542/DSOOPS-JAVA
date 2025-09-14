de52a5c9cd27fcd6b6440d4f0b57564eea21576c
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-09-14 18:33:17 +0530

    Delete Lec-36 directory

--- Lec-36/1.CommandLine.java
+++
@@ -1,8 +0,0 @@
-public  class cli {
-    public static void main(String[] args){
-        System.out.println("Number of arguments: "+ args.length);
-        for(String arg: args){
-            System.out.println(arg);
-        }
-    }
-}
--- Lec-36/2.Sum.java
+++
@@ -1,9 +0,0 @@
-public class Sum{
-    public static void main(String[] args) {
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
-        int num3 = Integer.parseInt(args[2]);
-        int sum = num1 + num2 + num3;
-        System.out.println("Sum = " + sum);
-    }
-}
