208f07cbe21a945603e778416d2cefb9a0455a3b
Rupali mangla <rupali0702.becse24@chitkara.edu.in>
2025-09-14 18:32:10 +0530

    First code of array

--- src/com/rupali/Day41/Array.java
+++ src/com/rupali/Day41/Array.java
@@ -1,4 +1,10 @@
 package com.rupali.Day41;
-
 public class Array {
+    public static void main(String[] args) {
+        int[] arr2 = new int[5];
+        int[] arr = { 12, 34, 13, 53, 57 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
