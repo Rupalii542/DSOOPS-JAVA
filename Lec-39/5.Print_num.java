546590efe8b38ee9eef16488205dcc4905530e65
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-09-14 18:33:51 +0530

    Delete Lec-39 directory

--- Lec-39/5.Print_num.java
+++
@@ -1,9 +0,0 @@
-package com.rupali.Day39;
-
-public class Print_num {
-    public static void main(String args[]){
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-    }
-}
