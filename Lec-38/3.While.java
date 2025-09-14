41dc6c83b6aa53eff9b0aad1f7f9962aa7a2782f
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-09-14 18:33:40 +0530

    Delete Lec-38 directory

--- Lec-38/3.While.java
+++
@@ -1,9 +0,0 @@
-public class While {
-    public static void main(String args[]){
-        int num=12;
-        while(num>0){
-            System.out.println(num);
-            num--;
-        }
-    }
-}
